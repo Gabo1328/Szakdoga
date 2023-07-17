6b9b8b1c55027364b73b3c36f83c4d113ca68eb6
Gábor Tussai <tussaig@mav.hu>
2023-07-17 15:34:39 +0200

    Felhasználók és járművek osztály létrehozása

---
+++ MavAutoKozm/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace MavAutoKozm.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        //navigációs property
+        public ICollection<Vehicle> Vehicles { get; set; }
+
+        //tovább fejlesztési lehetőségek (pl: vevő értékelés)
+
+    }
+}
---
+++ MavAutoKozm/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace MavAutoKozm.Models
+{
+    public class Vehicle
+    {
+        //PK
+        public int Id { get; set; }
+
+        //FK
+        public int UserId { get; set; }
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Type { get; set; }
+
+        public string Color { get; set; }
+
+        public string NumberPlate { get; set; }
+    }
+}
