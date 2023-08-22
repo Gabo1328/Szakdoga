19768dd05a124fd8c62b7d6045cd06dd8906caa5
Gábor Tussai <tussaig@mav.hu>
2023-08-22 18:26:40 +0200

    Szerepkör szerkesztés 1 (magyarázattal)

--- MavAutoKozm/Controllers/RoleController.cs
+++ MavAutoKozm/Controllers/RoleController.cs
@@ -39,8 +39,44 @@ namespace MavAutoKozm.Controllers
             return View();
         }
 
-        public ViewResult Edit() => View(_userManager.Users.Select(x => new UsersInRole { Name = x.UserName, UserId = x.Id, Member = true }));
+        //Edit függvénnyel egyszerű módon jelenítjük meg a view-t: =>
+        //A View függvénynek átadjuk a megjeleníteni kívánt UsersInRole listát.
+        //A _usermanagers.Users a weboldalra regisztrált összes user-t tartalmazza(táblanév:AspNetUsers)
+        //Végig gyalogolunk ezen a listán most épp a select utasítással
+        //A select azt csinálja hogy az adott lista elemeket felhasználhatom további értékadásra
+        //A new UsersInRole a végig gyaloglás során minden egyes listaelemen végig fog futni
+        //usersInRole 3 property-ét a végiggyaloglás során az x-nek nevezett adott listaelemből feltöltjük(Minden Member jelenleg "true" próbaként)
+        //Annyi elemű lista lesz a UsersInRole ahány eleme van a Users-nek
+        //a Select kifejezetten erre való: --Egy adott listán végig menve azokból valamilyen információt kiszedve teljesen új osztáy listát hozol létre--
+        public ViewResult Edit_Old() => View(_userManager.Users.Select(x => new UsersInRole { Name = x.UserName, UserId = x.Id, Member = true }));
         //new List<UsersInRole>());
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var lista = new List<UsersInRole>();
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                //Első megoldás
+                
+                foreach (var item in _userManager.Users)
+                {                   
+                    var member = await _userManager.IsInRoleAsync(item, role.Name);  
+                    var listaelem = new UsersInRole { Name = item.UserName, UserId = item.Id, Member = member };
+                    lista.Add(listaelem);
+                }
+
+                //Második megoldás
+                //var lista2 = _userManager.Users.Select(x => new UsersInRole
+                //{
+                //    Name = x.UserName,
+                //    UserId = x.Id,
+                //    Member = _userManager.IsInRoleAsync(x, role.Name).Result
+                //});
+            }
+            return View(lista);
+            
+        }
 
         public async Task<IActionResult> DeleteRole(string id)
         {
