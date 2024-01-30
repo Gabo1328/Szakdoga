ff25dfbcff250a52380974b13d9df0fc430f1bc0
Gábor Tussai <tussaig@mav.hu>
2024-01-30 14:03:23 +0100

    Teszt átnevezés

---
+++ MavAutoKozmTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTest_MavAutoKozm
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
