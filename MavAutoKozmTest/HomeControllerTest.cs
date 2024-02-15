283b62743c9e18e6ee6f16d2e89169f915e03f41
Gábor Tussai <tussaig@mav.hu>
2024-02-15 20:11:37 +0100

    DeleteOrder unit teszt

--- MavAutoKozmTest/HomeControllerTest.cs
+++ MavAutoKozmTest/HomeControllerTest.cs
@@ -75,5 +75,37 @@ namespace UnitTest_MavAutoKozm
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<IActionResult>(result);
         }
+
+        [Test]
+        public void DeleteOrderTest()
+        {
+            //Arrange
+            List<Orders> mockOrders = new List<Orders>{
+                new Orders
+                {
+                    AppUserId = 1,
+                    Category = 0,
+                    Ceramic = true,
+                    CompletedTime = DateTime.Now,
+                    Id = 1,
+                    Inner = true,
+                    OrderTime = DateTime.Now,
+                    Outer = false,
+                    Polish = true,
+                    Ppf = true,
+                    Price = 100000,
+                    Quality = 2,
+                    VehicleId = 2,
+                    Wax = false,
+                }};
+            _mockRepository.Setup(e => e.Orders).Returns(mockOrders);
+
+            //Action
+            var result = _homeController.DeleteOrder(1);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<Task<IActionResult>>(result);
+        }
     }
 }
