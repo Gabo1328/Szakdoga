1c553deaca7eaa50deefcac9c361b69e923b3ce7
Gábor Tussai <tussaig@mav.hu>
2024-02-15 17:49:32 +0100

    Repository létrehozás

---
+++ MavAutoKozm/Data/MavAutoKozmRepository.cs
@@ -0,0 +1,35 @@
+﻿using MavAutoKozm.Controllers;
+using MavAutoKozm.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MavAutoKozm.Data
+{
+    public interface IMavAutoKozmRepository
+    {
+        IEnumerable<AppUser> AppUsers { get;}
+        IEnumerable<Vehicle> Vehicles { get;}
+        IEnumerable<Orders> Orders { get;}
+
+
+        //void AddAppUser(AppUser AppUsers);
+
+        //void EditAppUser(AppUser AppUsers);
+
+        //void RemoveAppUser(AppUser AppUsers);
+    }
+
+    public class MavAutoKozmRepository : IMavAutoKozmRepository
+    {
+        private readonly MavAutoKozmDbContext _dbContext; //Ezzel éri el az adatbázist
+        public MavAutoKozmRepository(MavAutoKozmDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        public IEnumerable<AppUser> AppUsers => _dbContext.AppUsers.ToList();
+        public IEnumerable<Vehicle> Vehicles => _dbContext.Vehicles.ToList();
+        public IEnumerable<Orders> Orders => _dbContext.Orders.ToList();
+
+    }
+}
