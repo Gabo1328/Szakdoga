ddc77d4dfde5bf121d41c70890a556c2b93cd59d
Gábor Tussai <tussaig@mav.hu>
2024-02-16 10:49:24 +0100

    VehicleController tesztek 4

--- MavAutoKozmTest/VehiclesControllerTest.cs
+++ MavAutoKozmTest/VehiclesControllerTest.cs
@@ -85,6 +85,39 @@ namespace UnitTest_MavAutoKozm
             Assert.IsInstanceOf<Task<IActionResult>>(result);
         }
 
+        [Test]
+        public void DetailsTest()
+        {
+            //Arrange
+            List<Orders> mockOrders = new List<Orders>{
+                new Orders
+                {
+                    AppUserId = 1,
+                    Category = 0,
+                    Ceramic = true,
+                    CompletedTime = DateTime.Now,
+                    Id = 1,
+                    Inner = true,
+                    OrderTime = DateTime.Now,
+                    Outer = false,
+                    Polish = true,
+                    Ppf = true,
+                    Price = 100000,
+                    Quality = 2,
+                    VehicleId = 2,
+                    Wax = false,
+                }};
+
+            _mockRepository.Setup(e => e.Orders).Returns(mockOrders);
+
+            //Action
+            var result = _vehiclesController.Details(1);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<Task<IActionResult>>(result);
+        }
+
     }
 
 
