0b6fe830a8632c5dc40d30d72b2283c2fc59018f
Gábor Tussai <tussaig@mav.hu>
2024-02-22 10:58:37 +0100

    Unit teszt 17

--- MavAutoKozmTest/UserControllerTest.cs
+++ MavAutoKozmTest/UserControllerTest.cs
@@ -71,5 +71,92 @@ namespace UnitTest_MavAutoKozm
             Assert.IsInstanceOf<bool>(result);
             Assert.AreEqual(false, result);
         }
+
+        [Test]
+        public void CreateTestGet()
+        {
+            //Arrange
+
+            //Action
+            var result = _userController.Create();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+
+        [Test]
+        public void CreateTestPost()
+        {
+            //Arrange
+            List<AppUser> mockAppUsers = new List<AppUser>{
+                new AppUser
+                {
+                    AspNetUserId = "1de",
+                    Email = "www.w",
+                    FirstMidName = "Laci",
+                    LastName = "Varga",
+                    ID = 3,
+                    PhoneNumber = "1234567890",
+                    Vehicles = new List<Vehicle>()
+                }};
+            _mockRepository.Setup(e => e.AppUsers).Returns(mockAppUsers);
+            //Action
+            var result = _userController.Create();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+
+        [Test]
+        public async Task DeleteConfirmedTest_NullVehicle()
+        {
+            // Arrange
+            int mockAppUsersId = 1;
+
+            List<AppUser> mockAppUsers = null;
+
+            _mockRepository.Setup(x => x.AppUsers).Returns(mockAppUsers); // Simulate null list
+
+            // Action
+            var result = await _userController.DeleteConfirmed(mockAppUsersId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<ObjectResult>(result);
+        }
+
+        [Test]
+        public async Task DeleteConfirmedTest_SuccessfulDeletion()
+        {
+            // Arrange
+            int mockAppUsersId = 1; 
+            List<AppUser> mockAppUsers = new List<AppUser>{
+                new AppUser
+                {
+                    AspNetUserId = "1de",
+                    Email = "www.w",
+                    FirstMidName = "Laci",
+                    LastName = "Varga",
+                    ID = 3,
+                    PhoneNumber = "1234567890",
+                    Vehicles = new List<Vehicle>()
+                }};
+
+            _mockRepository.Setup(x => x.AppUsersDelete(It.IsAny<AppUser>())).Callback<AppUser>(appuser =>
+            {
+                // Simulate the deletion in the mock repository
+                _mockRepository.Object.AppUsers.Remove(appuser);
+            });
+
+            _mockRepository.Setup(x => x.AppUsers).Returns(mockAppUsers);
+            // Action
+            var result = await _userController.DeleteConfirmed(mockAppUsersId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+        }
     }
 }
