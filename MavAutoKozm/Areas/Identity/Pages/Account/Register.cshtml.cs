4b407ad117a02f7188883c8e89e5df2509fdc057
Gábor Tussai <tussaig@mav.hu>
2024-07-04 10:56:47 +0200

    Bejelentkezé és Regisztráció frontend

--- MavAutoKozm/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MavAutoKozm/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -86,7 +86,7 @@ namespace MavAutoKozm.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Jelszó")]
             public string Password { get; set; }
 
             /// <summary>
@@ -94,8 +94,8 @@ namespace MavAutoKozm.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Jelszó megerősítés")]
+            [Compare("Password", ErrorMessage = "A jelszavak nem egyeznek.")]
             public string ConfirmPassword { get; set; }
         }
 
